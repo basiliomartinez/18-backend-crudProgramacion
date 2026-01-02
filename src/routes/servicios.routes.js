8742a7f7f00cd0a065b71ea719752852ef94ebc6
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-01-02 11:07:08 -0300

    agregamos la logica necesaria para listar los servicios con un get

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { crearServicio, prueba } from "../controllers/servicios.controllers.js";
+import { crearServicio, listarServicios, prueba } from "../controllers/servicios.controllers.js";
 
 const router = Router()
 //aqui diseñamos todas las rutas para trabajar con los servicios
 //get-post-put o patch -delete 
 
 router.route("/test").get(prueba)
-router.route('/').post(crearServicio)
+router.route('/').post(crearServicio).get(listarServicios)
 
 export default router
