188208ea5b93f85b574768369948e588620ff1da
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-01-24 10:44:37 -0300

    agregamos la logica del controlador para crearUsuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,6 +1,23 @@
-export const crearUsuario = (req, res)=>{
-    
-}
-export const listarUsuario = (req, res)=>{
+import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
-}
+export const crearUsuario = async (req, res) => {
+  try {
+    //todo: Agregar validacion a los datos del usuario
+    //hasehar el password
+    const saltos = bcrypt.genSaltSync(10);
+    console.log(saltos);
+    const passwordHasheado = bcrypt.hashSync(req.body.password, saltos);
+    req.body.password = passwordHasheado;
+
+    const usuarioNuevo = new Usuario(req.body);
+    await usuarioNuevo.save();
+    res.status(201).json({ mensaje: "El usuario fue creado correctamente" });
+  } catch (error) {
+    console.error();
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar crear un usuario" });
+  }
+};
+export const listarUsuario = (req, res) => {};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { crearUsuario, listarUsuario } from "../controllers/usuarios.controllers.js";
+import {
+  crearUsuario,
+  listarUsuario,
+} from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
 //http://localhost:3000/api/servicios/test
 router.route("/").get(listarUsuario).post(crearUsuario);
 
-export default router
+export default router;
